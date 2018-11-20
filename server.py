a5362292edfd7ca11fe48b811edce5e03f21264a
Mert Lostar <mertlostar@gmail.com>
2018-11-21 02:10:09 +0300

    Squash unnecessary commits
    Fix connection issues
    
    
    Seperate queries from other statements
    
    
    Put templates in templates directory and fix typos
    
    Fix queries
    
    
    Fix typo

--- server.py
+++ server.py
@@ -6,29 +6,48 @@ import psycopg2 as dbapi2
 
 app = Flask(__name__)
 
+
 class DatabaseConnection:
     def __init__(self,url):
         self.cursor = dbapi2.connect(url).cursor()
-    def runStatements(self,statements):
-        self.cursor.execute(statements)
+
+    def run_statements(self,statements):
+        if self.cursor is not None:
+            self.cursor.execute(statements)
+        else:
+            print("Connection is None!\n")
+
+    def run_queries(self,queries):
+        if self.cursor is not None:
+            self.cursor.execute(queries)
+            return self.cursor.fetchall()
+        else:
+            print("Connection is None!\n")
+
+    def close(self):
+        self.cursor.close()
+
 
 @app.route("/")
 def home_page():
     return "Hello, world!"
+
+
 @app.route("/attacks")
-def attacks_page(connection=None):
-    attacks = connection.runStatements("SELECT * FROM ATTACKS")
+def attacks_page():
+    url = os.getenv("DATABASE_URL")
+    connection = DatabaseConnection(url)
+    attacks = connection.run_queries("SELECT * FROM ATTACKS;")
+    connection.close()
     return render_template('attacks.html',attacks=attacks)
 
 
 if __name__ == "__main__":
-
-    url = os.getenv("DATABASE_URL")
-    DBC = DatabaseConnection()
-    if url is None:
-        print("Failed connection to database")
-        sys.exit(1)
-    attacks_page(DBC)
     app.run()
+
+    if url is None:
+        print("Failed connection to database\n")
+        sys.exit(1)
+    attacks_page(dbc)
 
 
