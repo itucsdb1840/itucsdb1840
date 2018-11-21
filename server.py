0afe083e3e7ea23a085a4ae10ce5cfc9c65cec9d
Mert Lostar <mertlostar@gmail.com>
2018-11-21 11:21:52 +0300

    Initialize pages
    Remove close since connection is global now
    
    
    Modify global connection
    
    
    global variable change
    
    
    global variable
    
    
    Localize connections
    
    
    Modify Templates
    
    
    Delete tables before initializing and add index page
    
    
    Cascade deletions
    
    
    render index template

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 from flask import Flask,render_template
 import os
-import sys
 
 import psycopg2 as dbapi2
-
 app = Flask(__name__)
 
 
 class DatabaseConnection:
-    def __init__(self,url):
-        self.cursor = dbapi2.connect(url).cursor()
+    def __init__(self):
+        self.cursor=None
 
     def run_statements(self,statements):
         if self.cursor is not None:
@@ -24,30 +22,49 @@ class DatabaseConnection:
         else:
             print("Connection is None!\n")
 
+    def connect(self,url):
+        self.cursor = dbapi2.connect(url).cursor()
+
     def close(self):
         self.cursor.close()
 
 
 @app.route("/")
 def home_page():
-    return "Hello, world!"
+    return render_template('index.html')
 
 
 @app.route("/attacks")
 def attacks_page():
+    connection = DatabaseConnection()
     url = os.getenv("DATABASE_URL")
-    connection = DatabaseConnection(url)
+    connection.connect(url)
     attacks = connection.run_queries("SELECT * FROM ATTACKS;")
     connection.close()
-    return render_template('attacks.html',attacks=attacks)
+    return render_template('attacks.html',tuples=attacks)
+
+
+@app.route("/cities")
+def cities_page():
+    connection = DatabaseConnection()
+    url = os.getenv("DATABASE_URL")
+    connection.connect(url)
+    cities = connection.run_queries("SELECT * FROM CITIES;")
+    connection.close()
+    return  render_template('cities.html',tuples=cities)
+
+
+@app.route("/tgroups")
+def tgroups_page():
+    connection = DatabaseConnection()
+    url = os.getenv("DATABASE_URL")
+    connection.connect(url)
+    tgroups = connection.run_queries("SELECT * FROM TGROUPS;")
+    connection.close()
+    return render_template('tgroups.html',tuples=tgroups)
 
 
 if __name__ == "__main__":
     app.run()
-
-    if url is None:
-        print("Failed connection to database\n")
-        sys.exit(1)
-    attacks_page(dbc)
 
 
